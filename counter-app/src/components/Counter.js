ac825d981d13d40061f8974a693d29fdccc50704
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-11-14 16:43:25 +0300

    Update Counter.js

--- counter-app/src/components/Counter.js
+++ counter-app/src/components/Counter.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import { increment } from '../redux/counter/counterSlice'
+import { increment } from '../redux/counter/counterSlice';
 
 function Counter() {
   const countValue = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <div>
         <h1>
             { countValue }
         </h1>
+        <button>Decrement</button>
+        <button onClick={() => dispatch(increment())}>Increment</button>
     </div>
   )
 }
