834757dae960c5ab5ee6d8999e36deef0ab9ab08
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-11-14 16:11:19 +0300

    Update Counter.js

--- counter-app/src/components/Counter.js
+++ counter-app/src/components/Counter.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 function Counter() {
+  const countValue = useSelector((state) => state.counter.value);
+
   return (
     <div>
         <h1>
-            1
+            { countValue }
         </h1>
     </div>
   )
