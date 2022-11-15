e535a8292ba1fd0e49b534677241d038af7f8f34
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-11-15 16:59:28 +0300

    components

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,19 @@
 
 import './App.css';
+import Content from './components/Content';
+import Footer from './components/Footer';
 import Header from './components/Header';
 
 function App() {
   return (
-    <section className="todoapp">
-      <Header /> 
-    </section>
+    <>
+      <section className="todoapp">
+        <Header /> 
+        <Content />
+      </section>
+
+      <Footer />
+    </>
   );
 }
 
