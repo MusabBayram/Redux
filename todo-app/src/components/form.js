99bd49db694a6672004bcb6ade3854cfce0688d1
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-11-15 16:42:23 +0300

    Update form.js

--- todo-app/src/components/form.js
+++ todo-app/src/components/form.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function form() {
+function Form() {
   return (
     <form>
 		<input className="new-todo" placeholder="What needs to be done?" autoFocus />
@@ -8,4 +8,4 @@ function form() {
   )
 }
 
-export default form
+export default Form
