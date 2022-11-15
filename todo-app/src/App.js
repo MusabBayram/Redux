b561dc15030507619a3e19c899b27a819cb8dfeb
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-11-15 16:40:51 +0300

    Update App.js

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <section className="todoapp">
-    
+      <Header /> 
     </section>
   );
 }
