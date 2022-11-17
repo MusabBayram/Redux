dc06d7f742e118aadf546d30289d9fa252765cb6
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-11-17 17:57:27 +0300

    Update TodoList.js

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 function TodoList() {
+	const dispatch = useDispatch();
 	const items = useSelector((state) => state.todos.items);
 
   return (
