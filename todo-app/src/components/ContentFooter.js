d22bf4139777d567e7b0cff80a868a2b31f2fea4
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-11-21 14:06:26 +0300

    Update ContentFooter.js

--- todo-app/src/components/ContentFooter.js
+++ todo-app/src/components/ContentFooter.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeActiveFilter } from '../redux/todos/todosSlice';
 
 function ContentFooter() {
+  const dispatch = useDispatch()
   const items = useSelector((state) => state.todos.items);
   const itemLeft = items.filter((item) => !item.completed).length;
 
@@ -17,13 +19,13 @@ function ContentFooter() {
 
 		<ul className="filters">
 			<li>
-				<a href="#/" className={activeFilter === 'all' ? "selected" : ''}>All</a>
+				<a href="#/" className={activeFilter === 'all' ? "selected" : ''} onClick={() => dispatch(changeActiveFilter('all'))}>All</a>
 			</li>
 			<li>
-				<a href="#/" className={activeFilter === 'active' ? "selected" : ''}>Active</a>
+				<a href="#/" className={activeFilter === 'active' ? "selected" : ''} onClick={() => dispatch(changeActiveFilter('active'))}>Active</a>
 			</li>
 			<li>
-				<a href="#/" className={activeFilter === 'completed' ? "selected" : ''}>Completed</a>
+				<a href="#/" className={activeFilter === 'completed' ? "selected" : ''} onClick={() => dispatch(changeActiveFilter('completed'))}>Completed</a>
 			</li>
 		</ul>
 
