af70310eb30983523cbfe188b0e838fb65a8da22
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-11-22 14:39:01 +0300

    Update TodoList.js

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { toggle, destroy, selectTodos } from '../redux/todos/todosSlice';
+import { toggle, destroy, selectFilteredTodos } from '../redux/todos/todosSlice';
 
-let filtered = [];
 
 function TodoList() {
 	const dispatch = useDispatch();
-	const items = useSelector(selectTodos);
-	const activeFilter = useSelector((state) => state.todos.activeFilter);
+	const filteredTodos = useSelector(selectFilteredTodos);
 
 	const handleDestroy = (id) => {
 		if(window.confirm('Are you sure?')){
@@ -15,20 +13,16 @@ function TodoList() {
 		}
 	}
 
-	filtered = items;
-	if(activeFilter !== 'all'){
-		filtered = items.filter((todo) => activeFilter === 'active' ? todo.completed === false : todo.completed === true);
-	}
 
   return (
     <ul className="todo-list">
-			{items.map((item) => (
+			{filteredTodos.map((item) => (
 				<li key={item.id} className={item.completed ? 'completed' : ''}>
 					<div className="view">
 						<input 
 							className="toggle" 
 							type="checkbox" 
-							onChange={dispatch(toggle({id: item.id}))} 
+							onChange={() => dispatch(toggle({id: item.id}))} 
 							checked={item.completed}
 						/>
 						<label>{item.title}</label>
