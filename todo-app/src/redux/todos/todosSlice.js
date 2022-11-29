cad2f18cb90fdb997b5e060fa934d74ea33247e4
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-11-29 17:54:44 +0300

    some changes

--- todo-app/src/redux/todos/todosSlice.js
+++ todo-app/src/redux/todos/todosSlice.js
@@ -1,8 +1,9 @@
 import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 export const getTodosAsync = createAsyncThunk('todos/getTodosAsync', async () => {
-    const res = await fetch('http://localhost:7000/todos');
-    return await res.json();
+    const res = await axios('http://localhost:7000/todos');
+    return await res.data;
 });
 
 export const todosSlice = createSlice({
