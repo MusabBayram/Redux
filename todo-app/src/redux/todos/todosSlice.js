d4845f438c5ebd6be58ddda75902934aff8d656a
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-11-29 14:52:35 +0300

    Update todosSlice.js

--- todo-app/src/redux/todos/todosSlice.js
+++ todo-app/src/redux/todos/todosSlice.js
@@ -1,20 +1,16 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getTodosAsync = createAsyncThunk('todos/getTodosAsync', async () => {
+    const res = await fetch('http://localhost:7000/todos');
+    return await res.json();
+});
 
 export const todosSlice = createSlice({
     name: 'todos',
     initialState: {
-        items: [
-            {
-                id: '1',
-                title: 'Learn React',
-                completed: true,
-            },
-            {
-                id: '2',
-                title: 'Read a book',
-                completed: false,
-            }
-        ],
+        items: [],
+        isLoading: false,
+        error: null,
         activeFilter: 'all',
     },
     reducers: {
@@ -50,7 +46,20 @@ export const todosSlice = createSlice({
             const filtered = state.items.filter((item) => item.completed === false);
             state.items = filtered;
         }
-},
+    },
+    extraReducers: {
+        [getTodosAsync.pending]: (state, action) => {
+            state.isLoading = true;
+        },
+        [getTodosAsync.fulfilled]: (state, action) => {
+            state.items = action.payload;
+            state.isLoading = false;
+        },
+        [getTodosAsync.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.error = action.error.message;
+        }
+    }
 });
 
 export const selectTodos = (state) => state.todos.items;
