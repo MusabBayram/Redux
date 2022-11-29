8cb26880f6f9fc6ea520fa75579f58562f5df05a
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-11-29 16:16:00 +0300

    Update TodoList.js

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { toggle, destroy, selectFilteredTodos, getTodosAsync } from '../redux/todos/todosSlice';
+import Loading from './Loading';
 
 
 function TodoList() {
