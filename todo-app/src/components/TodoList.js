2f6bdf6e1b00284d6e1d22e4444fc0086f75d1b5
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-11-29 16:12:15 +0300

    Update TodoList.js

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { toggle, destroy, selectFilteredTodos } from '../redux/todos/todosSlice';
+import { toggle, destroy, selectFilteredTodos, getTodosAsync } from '../redux/todos/todosSlice';
 
 
 function TodoList() {
 	const dispatch = useDispatch();
 	const filteredTodos = useSelector(selectFilteredTodos);
+	const isLoading = useSelector((state) => state.todos.isLoading);
+
+	useEffect(() => {
+		dispatch(getTodosAsync());
+	}, [dispatch]);
 
 	const handleDestroy = (id) => {
 		if(window.confirm('Are you sure?')){
@@ -13,6 +18,9 @@ function TodoList() {
 		}
 	}
 
+	if(isLoading) {
+		return <Loading />;
+	}
 
   return (
     <ul className="todo-list">
