7e70ed4775925fef6e8c75c08ad38dbee9b78849
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-11-30 14:17:34 +0300

    Update form.js

--- todo-app/src/components/form.js
+++ todo-app/src/components/form.js
@@ -1,18 +1,18 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addTodo } from '../redux/todos/todosSlice';
+import { addTodoAsync } from '../redux/todos/todosSlice';
 
 function Form() {
   const [title, setTitle] = useState('');
 
   const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     if(!title) return;
     
     e.preventDefault();
 
-    dispatch(addTodo({ title }))
+    await dispatch(addTodoAsync({ title }))
     
     setTitle('')
   }
