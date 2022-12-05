04412a5cf728a461da74ee3ede019809eb03676f
Musab Bahadır Bayram <34100687+MusabBayram@users.noreply.github.com>
2022-12-05 14:13:46 +0300

    last commit

--- todo-app/src/components/ContentFooter.js
+++ todo-app/src/components/ContentFooter.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import { changeActiveFilter, clearCompleted, selectTodos } from '../redux/todos/services';
+import { changeActiveFilter, clearCompleted, selectTodos } from '../redux/todos/todosSlice';
 
 function ContentFooter() {
   const dispatch = useDispatch()
--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { removeTodoAsync, selectFilteredTodos, getTodosAsync, toggleTodoAsync } from '../redux/todos/services';
+import { removeTodoAsync, getTodosAsync, toggleTodoAsync } from '../redux/todos/services';
+import { selectFilteredTodos } from '../redux/todos/todosSlice';
 import Error from './Error';
 import Loading from './Loading';
 
